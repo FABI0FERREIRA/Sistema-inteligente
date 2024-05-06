c9b7d680a96b6d632f3bf2ca51de3abe8cd4c6c1
FABI0FERREIRA <154641904+FABI0FERREIRA@users.noreply.github.com>
2024-05-05 21:12:07 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,5 @@
+from bing_image_downloader import downloader
+downloader.download('motorcycle helmet', limit=100,  output_dir=r'C:\Users\fabfe\PycharmProjects\OpenCVScript_4\pythonProject1\imagem', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+
+
+
